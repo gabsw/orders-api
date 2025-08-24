a2fd476ae3726d18961390114bb12db4762c3d10
gabsw <sw.gabrielasantos@gmail.com>
2025-08-24 15:03:48 +0100

    Adds blank project

---
+++ demo/src/main/java/orders/api/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package orders.api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
